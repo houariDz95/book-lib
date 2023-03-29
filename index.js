c911a4945cca4e0b8974e299f2477bfe08b76fb7
houariDz95 <houarieddrief7@gmail.com>
2023-03-29 13:44:39 +0100

    port fix

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import books from './routes/books.js'
 
 const app = express();
-const PORT = 5000 || process.env.PORT
+const PORT = process.env.PORT || 5000
 
 app.get('/', (req, res) => {res.send("welcome to hundawi scraper api")})
 app.use('/api/books', books)
