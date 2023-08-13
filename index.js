936361ee4b19c7e40b571a37622640b9f3896db6
houaridz95 <houarieddrief7@gmail.com>
2023-08-13 13:29:17 +0200

    remove books endpoint

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ import Categories from './routes/categories.js';
 import Search from './routes/search.js'
 import New from './routes/new.js'
 import CatList from './routes/catList.js'
-
+import { getBook } from './controllers/getBook.js';
 const app = express();
 const PORT = process.env.PORT || 5000
 
 app.get('/', (req, res) => { res.send("welcome to oumat-iqra library api") })
 
 
-//app.use('/books', Books)
+app.use('/books', Books)
 app.use('/contributors', Author)
 app.use('/categories', Categories);
 app.use('/search', Search);
--- routes/books.js
+++ routes/books.js
@@ -1,11 +1,11 @@
 import express from "express";
-import {getBooks} from '../controllers/getBooks.js'
+//import {getBooks} from '../controllers/getBooks.js'
 import {getBook} from '../controllers/getBook.js'
 
 const router = express.Router();
 
 router.get("/:id", getBook);
-router.get("/", getBooks);
+//router.get("/", getBooks);
 
 
 export default router;
